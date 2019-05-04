7db73bf69ebe1eec49b90eadfc28abf3c2e0009a
Mike Slaby <mikeslaby3@gmail.com>
2019-05-04 16:29:52 -0700

    Added Bands in Town API to pull concert information for inputted artist

--- liri.js
+++ liri.js
@@ -7,7 +7,8 @@ var axios = require('axios');
 var fs = require('fs');
 var keys = require('./keys.js');
 
-// var spotify = new spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 function getUserArguments() {
     return process.argv.slice(2);
@@ -74,8 +75,71 @@ searchMovie();
 // Spotify API
 // ====================
 
+
+function findSongData() {
+    spotify
+      .search({ type: 'track', query: 'All the Small Things' })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+}
+
+function searchSong() {
+    const songTitle = getSearchTerm();
+
+    if (getSearchType() === 'spotify-this-song') {
+      console.log('Finding song data!')
+      if (songTitle === '') {
+        findMovieData('Mr. Nobody')
+      } else {
+        findMovieData(songTitle);
+      }
+    }
+}
+
 // ====================
 // Bands In Town API
 // ====================
 
-// var bandUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+function buildArtistUrl(artistName) {
+    return 'https://rest.bandsintown.com/artists/' + artistName + '/events?app_id=codingbootcamp'
+} 
+
+function displayConcertData(concertData) {
+    const concert = concertData
+    console.log('\n');
+    console.log('Venue: ' + concert.venue.name);
+    console.log('Location: ' + concert.venue.city);
+    console.log('Concert Date: ' + concert.datetime);
+    console.log('\n');
+}
+
+function findConcertData(artistName) {
+    axios
+      .get(buildArtistUrl(artistName))
+      .then(function (response) {
+        const concertData = response.data[0];
+        displayConcertData(concertData);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+}
+
+function searchArtist() {
+    const artistName = getSearchTerm();
+
+    if (getSearchType() === 'concert-this') {
+      console.log('Finding concert data!')
+      if (artistName === '') {
+        findConcertData('Blink 182')
+      } else {
+        findConcertData(artistName);
+      }
+    }
+}
+
+searchArtist();
