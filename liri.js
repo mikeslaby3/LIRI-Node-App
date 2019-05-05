c9777a6c3a1a6577e1ffceaa4a332744c3c8434d
Mike Slaby <mikeslaby3@gmail.com>
2019-05-04 17:47:23 -0700

    Implemented do-what-it-says argument that reads text file and performs that command

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@
 // ==============================
 
 require('dotenv').config();
+
 var axios = require('axios');
 var fs = require('fs');
 var keys = require('./keys.js');
@@ -28,7 +29,7 @@ function getSearchTerm() {
 
 function buildMovieUrl(movieTitle) {
     return 'http://www.omdbapi.com/?t=' + movieTitle + '&apikey=4aba57dd'
-} 
+}
 
 function displayMovieData(movieData) {
     const movie = movieData
@@ -46,26 +47,26 @@ function displayMovieData(movieData) {
 
 function findMovieData(movieTitle) {
     axios
-      .get(buildMovieUrl(movieTitle))
-      .then(function (response) {
-        const movieData = response.data;
-        displayMovieData(movieData);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+        .get(buildMovieUrl(movieTitle))
+        .then(function (response) {
+            const movieData = response.data;
+            displayMovieData(movieData);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 }
 
 function searchMovie() {
     const movieTitle = getSearchTerm();
 
     if (getSearchType() === 'movie-this') {
-      console.log('Finding movie data!')
-      if (movieTitle === '') {
-        findMovieData('Mr. Nobody')
-      } else {
-        findMovieData(movieTitle);
-      }
+        console.log('Finding movie data!')
+        if (movieTitle === '') {
+            findMovieData('Mr. Nobody')
+        } else {
+            findMovieData(movieTitle);
+        }
     }
 }
 
@@ -75,30 +76,32 @@ searchMovie();
 // Spotify API
 // ====================
 
-
-function findSongData() {
-    spotify
-      .search({ type: 'track', query: 'All the Small Things' })
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
+function findSongData(songTitle) {
+    spotify.search({ type: 'track', query: songTitle }, function (err, data) {
+        if (err) {
+            console.log("Can't find the song you're looking for")
+            findSongData('The Sign by Ace of Base');
+            return;
+        }
+        console.log('\n');
+        console.log('Song Title: ' + data.tracks.items[0].name);
+        console.log('Artist(s): ' + data.tracks.items[0].artists[0].name);
+        console.log('Album: ' + data.tracks.items[0].album.name);
+        console.log('Preview: ' + data.tracks.items[0].preview_url);
+        console.log('\n');
+    });
 }
 
 function searchSong() {
     const songTitle = getSearchTerm();
 
     if (getSearchType() === 'spotify-this-song') {
-      console.log('Finding song data!')
-      if (songTitle === '') {
-        findMovieData('Mr. Nobody')
-      } else {
-        findMovieData(songTitle);
-      }
+        console.log('Finding song data!')
+        findSongData(songTitle);
     }
 }
+
+searchSong();
 
 // ====================
 // Bands In Town API
@@ -106,7 +109,7 @@ function searchSong() {
 
 function buildArtistUrl(artistName) {
     return 'https://rest.bandsintown.com/artists/' + artistName + '/events?app_id=codingbootcamp'
-} 
+}
 
 function displayConcertData(concertData) {
     const concert = concertData
@@ -119,27 +122,73 @@ function displayConcertData(concertData) {
 
 function findConcertData(artistName) {
     axios
-      .get(buildArtistUrl(artistName))
-      .then(function (response) {
-        const concertData = response.data[0];
-        displayConcertData(concertData);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+        .get(buildArtistUrl(artistName))
+        .then(function (response) {
+            const concertData = response.data[0];
+            displayConcertData(concertData);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 }
 
 function searchArtist() {
     const artistName = getSearchTerm();
 
     if (getSearchType() === 'concert-this') {
-      console.log('Finding concert data!')
-      if (artistName === '') {
-        findConcertData('Blink 182')
-      } else {
-        findConcertData(artistName);
-      }
+        console.log('Finding concert data!')
+        if (artistName === '') {
+            findConcertData('Blink 182')
+        } else {
+            findConcertData(artistName);
+        }
     }
 }
 
 searchArtist();
+
+// ====================
+// Text File Command
+// ====================
+
+function findTextFileData() {
+
+    fs.readFile("random.txt", "utf8", function(err, file) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(file);
+        
+        let textFile = file.split(",");
+        const randomTextSearchType = textFile[0];
+        const randomTextSearchTerm = textFile[1];
+
+        switch (randomTextSearchType) {
+            case "concert-this":
+                findConcertData(randomTextSearchTerm)
+                break;
+        
+            case "spotify-this-song":
+                findSongData(randomTextSearchTerm);
+                break;
+        
+            case "movie-this":
+                findMovieData(randomTextSearchTerm);
+                break;
+        
+            default:
+                console.log("Can't read random text format");
+        }
+        
+    });
+    
+}
+
+function searchTextFile() {
+    if (getSearchType() === 'do-what-it-says') {
+        console.log('Finding what the text file says')
+        findTextFileData();
+    }
+}
+
+searchTextFile();
